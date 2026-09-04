d09d9abcd5d513326e23350311b6e0eb6091d24f
iyasin933 <Imranyasin@faslogistics.co.uk>
2026-09-04 10:22:38 +0500

    chore: update version to 1.1.2 in app.config.js

--- app.config.js
+++ app.config.js
@@ -32,7 +32,7 @@ module.exports = () => {
       name: 'DropYou',
       slug: 'dropyou',
       scheme: googleIosUrlScheme ? ['dropyou', googleIosUrlScheme] : 'dropyou',
-      version: '1.1.1',
+      version: '1.1.2',
       orientation: 'portrait',
       icon: './assets/images/D-app-logo.png',
       userInterfaceStyle: 'automatic',
