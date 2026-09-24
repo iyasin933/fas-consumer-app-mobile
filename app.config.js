d51427b5c0bf8505ca17c067da1b2b1b0cec7111
imran-yasin <imranyasin944@gmail.com>
2026-09-25 00:31:17 +0500

    fix: update version to 1.1.5 in app.config.js and package.json

--- app.config.js
+++ app.config.js
@@ -32,7 +32,7 @@ module.exports = () => {
       name: 'DropYou',
       slug: 'dropyou',
       scheme: googleIosUrlScheme ? ['dropyou', googleIosUrlScheme] : 'dropyou',
-      version: '1.1.4',
+      version: '1.1.5',
       orientation: 'portrait',
       icon: './assets/images/D-app-logo.png',
       userInterfaceStyle: 'automatic',
